bd40f841cb8fef414bd2ee8edf6e79b081bea002
Adam Borocz <boroczadam@gmail.com>
2013-04-21 19:04:34 +0100

    Update metadata with missing support and attributes

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      "Installs/Configures s3cmd for a given set of users"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
+supports 'ubuntu', ">= 10.04"
+
 attribute "s3cmd/users",
   :display_name => "S3cmd Users",
   :description => "The list of user accounts to setup s3cmd for",
@@ -17,3 +19,15 @@ attribute "s3cmd/bucket_location",
   :description => "The Amazon S3 zone we're connecting to",
   :type => "string",
   :default => "EU"
+
+attribute "s3cmd/access_key",
+  :display_name => "AWS Access Key",
+  :description => "AWS Access Key",
+  :type => "string",
+  :default => ""
+
+attribute "s3cmd/secret_key",
+  :display_name => "AWS Secret Key",
+  :description => "AWS Secret Key",
+  :type => "string",
+  :default => ""
